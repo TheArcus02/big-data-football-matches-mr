222c111f0b710a37bc2464586941c77e667ba854
Mikołaj Urbaniak <m.urbaniak.inf@gmail.com>
2026-04-08 21:35:18 +0200

    fixed run_mr.sh

--- run_mr.sh
+++ run_mr.sh
@@ -5,4 +5,4 @@ OUT_DIR=$2
 
 hadoop fs -rm -r -f $OUT_DIR
 
-hadoop jar target/footballmatches.jar com.example.bigdata.FootballMatches $IN_DIR $OUT_DIR cluster
+hadoop jar target/footballmatches.jar $IN_DIR $OUT_DIR cluster
